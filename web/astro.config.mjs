af8fba67ca8f130bace6b69d1b1ad5e405ec2616
Justin Nappi <jtn8001@gmail.com>
2025-06-27 11:05:38 -0400

    feat: migrate components to Tailwind CSS and add coach card component

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 import sanity from "@sanity/astro"
 import react from "@astrojs/react"
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [
@@ -14,5 +16,9 @@ export default defineConfig({
     }),
     react()
 
-  ]
+  ],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
